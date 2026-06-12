3a3334b3eb464a4bd1bbedfb9681193a20b56be6
timi <timi@local>
2026-06-12 12:03:18 +0800

    chore: auto sync projects/yujing 2026-06-12 12:03

--- miniapp/src/config.js
+++ miniapp/src/config.js
@@ -1,3 +1,3 @@
-const defaultBaseUrl = 'http://localhost:3000';
+const defaultBaseUrl = 'https://gz-zoo-api.usthxo.cn';
 
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL || defaultBaseUrl;
